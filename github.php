53082d8fa83f80fcd68b63db1f68e3c69457501e
MUQ webmaster <webmaser@muq.mit.edu>
2015-09-15 13:11:09 -0400

    Fixed github webhook

--- github.php
+++ github.php
@@ -1,10 +1,8 @@
 <?php
 
-if ($_POST['payload']) {
+if (isset($_POST['payload'])) {
   $result = shell_exec('cd /home/webmaster/muq-website && git reset --hard HEAD && git pull');
-  echo '<pre>';
   echo $result;
-  echo '</pre>';
 }
 
 ?>
