03efb76fceb575d346061524006969007984312c
Matt Parno <mparno@mit.edu>
2015-09-16 07:42:04 -0800

    Updated about page.

--- pages/about.php
+++ pages/about.php
@@ -1,17 +1,55 @@
 <?php
 $tpl['page_name'] = 'About';
 $tpl['tab'] = 'About';
+
+$people = json_decode(file_get_contents('json/people.json'), true);
+
+// helper function for deciding whether to hide a section
+function count_people($type) {
+  global $people;
+  $count = 0;
+  foreach($people as $person) {
+    if ($person['type'] == $type) 
+      $count++;
+  }
+  return $count;
+}
+// outputs an itemized list of people based on type
+// visitors and UROP students get special treatment 
+function people_filter($type) {
+  echo '<div class="row">';
+
+  echo '<ul class="'.$type.' clearfix">';
+  global $people;
+  $count = 0;
+  foreach($people as $person) {
+    if ($person['type'] == $type) {
+      echo '<div class="col-xs-12 col-sm-6 col-md-4">';
+      echo '<h3 class="text-center">'.$person['name'].'</h3>';
+      echo '<img src="/images/people/'.$person['url'].'.png" alt="'.$person['name'].'" title="'.$person['name'].'" class="img-responsive center-block" />';
+      echo '</div>';
+    }
+  }
+  echo '</div>';
+
+}
+
 ?>
-<h2>Where we come from</h2>
-<p> Started internally around 2011, MUQ originally started as an outlet for some of the advanced research going on in Professor Youssef Marzouk's <a href="http://uqgroup.mit.edu/">uncertainty quantification group</a> at MIT.  Since then, we have grown substantially but we still strive to provide a pipeline between cutting-edge algorithmic work and challenging scientific applications.  We are also part of the <a href="http://www.quest-scidac.org/">QUEST</a> SciDAC institute.</p>
-<h2>Core Team</h2>
-<ul>
-<li><a href="http://mparno.mit.edu/">Matthew Parno</a></li>
-<li>Andrew Davis</li>
-<li>Patrick Conrad</li>
-</ul>
-<h2>Contributors</h2>
-<ul>
-<li>Chi Feng</li>
-<li><a href="http://www.limitcycle.it/dabi/">Daniele Bigoni</a></li>
-</ul>
+
+<div class="row row-offcanvas row-offcanvas-right">
+
+	<div class="jumbotron">
+		<h1>Where are we?</h1>
+		<p> MUQ is primarily developed by graduate students and postdocs in Professor Youssef Marzouk's uncertainty quantification group at MIT.  MUQ was originally started internally around 2011 to provide a usable outlet for our research.  Since then, we have grown substantially and now provide a much larger framework for developing new algorithms and coupling them with challenging scientific applications.  We are also part of the <a href="http://www.quest-scidac.org/">QUEST</a> SciDAC institute.</p>
+	</div>
+	
+	
+	
+	<h2>Core Team</h2>
+	<?php people_filter('core'); ?>
+	
+	<h2>Contributors</h2>
+	<?php people_filter('contributor'); ?>
+
+</div>
+<hr>
