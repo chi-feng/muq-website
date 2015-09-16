75a015b0c8249343a67f0bd2aa72a1262f41e91e
Chi Feng <email.chi.feng@gmail.com>
2015-09-15 20:36:45 -0400

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -7,38 +7,40 @@ if ($tpl['hide_sidebar'] == false) {
 <div class="panel-heading">Test Status</div>
 <div class="panel-body">
 <h4>Develop Branch:</h4>
-<table style="width:100%">
-	<tr>
-		<th> OS </th>
-		<th> Compiler </th>
-	</tr>
-	<tr>
-		<td>OSX</td>
-		<td align="center">Clang</td>
-        <td><a href="https://acdl.mit.edu/csi/job/MUQ_Develop_Nightly/"><img alt="Test Status" src="https://acdl.mit.edu/csi/buildStatus/icon?job=MUQ_Develop_Nightly/builddir=release_clang,buildnode=macys"></a></td>
-    </tr>
-    <tr>
-		<td>Ubuntu</td>
-		<td align="center">Clang</td>
-        <td><a href="https://acdl.mit.edu/csi/job/MUQ_Develop_Nightly/"><img alt="Test Status" src="https://acdl.mit.edu/csi/buildStatus/icon?job=MUQ_Develop_Nightly/builddir=release_clang,buildnode=reynolds"></a></td>
-    </tr>
-    <tr>
-		<td>Ubuntu</td>
-		<td align="center">g++ 4.7</td>
-        <td><a href="https://acdl.mit.edu/csi/job/MUQ_Develop_Nightly/"><img alt="Test Status" src="https://acdl.mit.edu/csi/buildStatus/icon?job=MUQ_Develop_Nightly/builddir=release_gnu47,buildnode=reynolds"></a></td>
-    </tr>
-    <tr>
-		<td>Ubuntu</td>
-		<td align="center">g++ 4.8</td>
-        <td><a href="https://acdl.mit.edu/csi/job/MUQ_Develop_Nightly/"><img alt="Test Status" src="https://acdl.mit.edu/csi/buildStatus/icon?job=MUQ_Develop_Nightly/builddir=release_gnu48,buildnode=reynolds"></a></td>
-    </tr>
-    <tr>
-		<td>Ubuntu</td>
-		<td align="center">g++ 4.9</td>
-        <td><a href="https://acdl.mit.edu/csi/job/MUQ_Develop_Nightly/"><img alt="Test Status" src="https://acdl.mit.edu/csi/buildStatus/icon?job=MUQ_Develop_Nightly/builddir=release_gnu49,buildnode=reynolds"></a></td>
-    </tr>
+<table class="table">
+<thead>
+<tr><th>OS</th>
+<th>Compiler</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>OSX</td>
+<td>Clang</td>
+<td><a href="https://acdl.mit.edu/csi/job/MUQ_Develop_Nightly/"><img alt="Test Status" src="https://acdl.mit.edu/csi/buildStatus/icon?job=MUQ_Develop_Nightly/builddir=release_clang,buildnode=macys"></a></td>
+</tr>
+<tr>
+<td>Ubuntu</td>
+<td>Clang</td>
+<td><a href="https://acdl.mit.edu/csi/job/MUQ_Develop_Nightly/"><img alt="Test Status" src="https://acdl.mit.edu/csi/buildStatus/icon?job=MUQ_Develop_Nightly/builddir=release_clang,buildnode=reynolds"></a></td>
+</tr>
+<tr>
+<td>Ubuntu</td>
+<td>g++ 4.7</td>
+<td><a href="https://acdl.mit.edu/csi/job/MUQ_Develop_Nightly/"><img alt="Test Status" src="https://acdl.mit.edu/csi/buildStatus/icon?job=MUQ_Develop_Nightly/builddir=release_gnu47,buildnode=reynolds"></a></td>
+</tr>
+<tr>
+<td>Ubuntu</td>
+<td>g++ 4.8</td>
+<td><a href="https://acdl.mit.edu/csi/job/MUQ_Develop_Nightly/"><img alt="Test Status" src="https://acdl.mit.edu/csi/buildStatus/icon?job=MUQ_Develop_Nightly/builddir=release_gnu48,buildnode=reynolds"></a></td>
+</tr>
+<tr>
+<td>Ubuntu</td>
+<td>g++ 4.9</td>
+<td><a href="https://acdl.mit.edu/csi/job/MUQ_Develop_Nightly/"><img alt="Test Status" src="https://acdl.mit.edu/csi/buildStatus/icon?job=MUQ_Develop_Nightly/builddir=release_gnu49,buildnode=reynolds"></a></td>
+</tr>
+</tbody>
 </table>
-
 </div>
 </div>
 <div class="panel panel-default">
