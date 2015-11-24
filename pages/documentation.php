6904e15c59466a7bf5a2c2a64be949102e7b2ab7
Matt Parno <mparno@mit.edu>
2015-11-24 18:23:39 -0500

    Made some descriptions bold.

--- pages/documentation.php
+++ pages/documentation.php
@@ -3,5 +3,7 @@ $tpl['page_name'] = 'Documentation';
 $tpl['tab'] = 'Documentation';
 ?>
 <h3>Documentation</h3>
-<p>Doxygen documentation for the develop branch can be found at <a href="http://muq.mit.edu/develop-docs/">muq.mit.edu/develop-docs</a></p>
-<p>Similarly, doxygen documentation for the master branch can be found at <a href="http://muq.mit.edu/master-docs/">muq.mit.edu/master-docs</a></p>
+<ul>
+<li>Doxygen documentation for the <b>develop</b> branch can be found at <a href="http://muq.mit.edu/develop-docs/">muq.mit.edu/develop-docs</a></li>
+<li>Similarly, doxygen documentation for the <b>master</b> branch can be found at <a href="http://muq.mit.edu/master-docs/">muq.mit.edu/master-docs</a></li>
+</ul>
