ffc97a7e33d1bfefd941f4537cd92f0e605451aa
Matt Parno <mparno@mit.edu>
2015-11-24 18:20:26 -0500

    Updated links to doxygen-generated documentation.

--- pages/documentation.php
+++ pages/documentation.php
@@ -3,4 +3,5 @@ $tpl['page_name'] = 'Documentation';
 $tpl['tab'] = 'Documentation';
 ?>
 <h3>Documentation</h3>
-<p>Another webhook test</p>
+<p>Doxygen documentation for the develop branch can be found at <a href="http://muq.mit.edu/develop-docs/">muq.mit.edu/develop-docs</a></p>
+<p>Similarly, doxygen documentation for the master branch can be found at <a href="http://muq.mit.edu/master-docs/">muq.mit.edu/master-docs</a></p>
