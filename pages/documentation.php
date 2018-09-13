7a5686ab7e3764527e7a25b6becb69809fd37b62
Matthew Parno <Matthew.D.Parno.civ@mail.mil>
2018-09-13 08:04:07 -0400

    Added link for MUQ2 docs.

--- pages/documentation.php
+++ pages/documentation.php
@@ -3,7 +3,16 @@ $tpl['page_name'] = 'Documentation';
 $tpl['tab'] = 'Documentation';
 ?>
 <h3>Documentation</h3>
+
+MUQ is in the middle of a transition from the original "MUQ1" into a more user friendly and efficient version MUQ2.
+<ul>
+<li>MUQ2 Documentation:</li>
+<ul>
+<li>Doxygen documentation can be found at <a href="https://mituq.bitbucket.io/">mituq.bitbucket.io</a></li>
+<ul>
+<li>MUQ1 Documentation:</li>
 <ul>
 <li>Doxygen documentation for the <b>develop</b> branch can be found at <a href="http://muq.mit.edu/develop-docs/">muq.mit.edu/develop-docs</a></li>
 <li>Similarly, doxygen documentation for the <b>master</b> branch can be found at <a href="http://muq.mit.edu/master-docs/">muq.mit.edu/master-docs</a></li>
+</ul>
 </ul>
